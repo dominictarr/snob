d29a53bc965dca3c2b3deacbe0a77726b5810321
Dominic Tarr <dominic.tarr@gmail.com>
2012-03-14 12:32:23 +1300

    wow, down to 200 lines

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 function head (a) {
   return a[0]
 }
@@ -28,16 +27,14 @@ function any(ary, test) {
 
 function equal(a, b) {
   if(a.length != b.length) return false
-  for(var i in a) {
+  for(var i in a)
     if(a[i] !== b[i]) return false
-  }
   return true
 }
 
 function getArgs(args) {
   return args.length == 1 ? args[0] : [].slice.call(args)
 }
-
 
 // return the index of the element not like the others, or -1
 function oddElement(ary, cmp) {
@@ -62,10 +59,9 @@ function oddElement(ary, cmp) {
 }
 
 exports.lcs = 
-function lcs(a, b) {
+function lcs() {
   var cache = {}
-  var args = [].slice.call(arguments)
-  var lcses = []
+  var args = getArgs(arguments)
  
   function key (a,b){
     return a.length + ':' + b.length
@@ -86,21 +82,17 @@ function lcs(a, b) {
     }
   }
 
-  if(!args.length) return []
-  if(args.length == 1) return args.shift()
   if(args.length > 2) {
     //if called with multiple sequences
     //recurse, since lcs(a, b, c, d) == lcs(lcs(a,b), lcs(c,d))
     args.push(lcs(args.shift(), args.shift()))
-    return lcs.apply(null, args)
+    return lcs(args)
   }
-  return recurse.apply(null, args)
+  return recurse(args[0], args[1])
 }
 
 // given n sequences, calc the lcs, and then chunk strings into stable and unstable sections.
-// chunk(sequences, build)
-// build should be (stable, unstable)
-// either stable or unstable will be passed. but never both.
+// unstable chunks are passed to build
 exports.chunk =
 function (q, build) {
   var q = q.map(function (e) { return e.slice() })
@@ -134,11 +126,9 @@ exports.diff =
 function (a, b) {
   var changes = []
   exports.chunk([a, b], function (index, unstable) {
-    if(unstable) {
-      var del = unstable.shift().length
-      var insert = unstable.shift()
-      changes.push([index, del].concat(insert))
-    }
+    var del = unstable.shift().length
+    var insert = unstable.shift()
+    changes.push([index, del].concat(insert))
   })
   return changes
 }
@@ -163,16 +153,10 @@ exports.diff3 = function () {
   var args = getArgs(arguments)
   var r = []
   exports.chunk(args, function (index, unstable) {
-    if(unstable) {
-      unstable = unstable.slice()
-    //  var _o = unstable.splice(1, 1)[0]
-      var mine = unstable[0]
-      var insert = resolve(unstable)
-      del = mine.length
-      if(equal(mine, insert))
-        return 
-      r.push([index, del].concat(insert)) 
-    }
+    var mine = unstable[0]
+    var insert = resolve(unstable)
+    if(equal(mine, insert)) return 
+    r.push([index, mine.length].concat(insert)) 
   })
   return r
 }
@@ -183,25 +167,30 @@ var rules = [
     //put the concestor first
     changes.unshift(changes.splice(1,1)[0])
     var oddi = oddElement(changes, equal)
-    if(oddi == 0) {// concestor was different
+    if(oddi == 0) // concestor was different
       return changes[1]
-    }
-    else if (oddi == -1) {
-      //if there is only one non empty change thats okay.
-      //else full confilct
-      for (var i = 1, nonempty; i < changes.length; i++)
-        if(changes[i].length) 
-          if(!nonempty) nonempty = changes[i]
-          else return // full conflict
-      return nonempty
-    } else
-      return changes[oddi]
- }
+    if (~oddi)
+      return changes[oddi] 
+  },
+  //i've implemented this as a seperate rule,
+  //because I had second thoughts about this.
+  function insertMergeOverDelete (changes) {
+    changes = changes.slice()
+    changes.splice(1,1)// remove concestor
+    
+    //if there is only one non empty change thats okay.
+    //else full confilct
+    for (var i = 0, nonempty; i < changes.length; i++)
+      if(changes[i].length) 
+        if(!nonempty) nonempty = changes[i]
+        else return // full conflict
+    return nonempty
+  }
 ]
 
 function resolve (changes) {
   var l = rules.length
-  for (var i in rules) {
+  for (var i in rules) { // first
     var c = rules[i](changes)
     if(c) return c
   }
