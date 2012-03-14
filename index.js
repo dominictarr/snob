d13448b6758b68c715d65e0590211ee917c36b99
Dominic Tarr <dominic.tarr@gmail.com>
2012-03-14 13:22:46 +1300

    export a function, with injectable equal function

--- index.js
+++ index.js
@@ -58,143 +58,146 @@ function oddElement(ary, cmp) {
   return c == 0 ? 0 : -1
 }
 
-exports.lcs = 
-function lcs() {
-  var cache = {}
-  var args = getArgs(arguments)
- 
-  function key (a,b){
-    return a.length + ':' + b.length
+module.exports = function (exports, deps) {
+  exports = exports || {}  
+  exports.lcs = 
+  function lcs() {
+    var cache = {}
+    var args = getArgs(arguments)
+   
+    function key (a,b){
+      return a.length + ':' + b.length
+    }
+
+    function recurse (a, b) {
+
+      if(!a.length || !b.length) return []
+      //avoid exponential time by caching the results
+      if(cache[key(a, b)]) return cache[key(a, b)]
+
+      if(a[0] == b[0])
+        return [head(a)].concat(recurse(tail(a), tail(b)))
+      else { 
+        var _a = recurse(tail(a), b)
+        var _b = recurse(a, tail(b))
+        return cache[key(a,b)] = _a.length > _b.length ? _a : _b  
+      }
+    }
+
+    if(args.length > 2) {
+      //if called with multiple sequences
+      //recurse, since lcs(a, b, c, d) == lcs(lcs(a,b), lcs(c,d))
+      args.push(lcs(args.shift(), args.shift()))
+      return lcs(args)
+    }
+    return recurse(args[0], args[1])
   }
 
-  function recurse (a, b) {
+  // given n sequences, calc the lcs, and then chunk strings into stable and unstable sections.
+  // unstable chunks are passed to build
+  exports.chunk =
+  function (q, build) {
+    var q = q.map(function (e) { return e.slice() })
+    var lcs = exports.lcs.apply(null, q)
+    var all = [lcs].concat(q)
 
-    if(!a.length || !b.length) return []
-    //avoid exponential time by caching the results
-    if(cache[key(a, b)]) return cache[key(a, b)]
+    function matchLcs (e) {
+      return last(e) == last(lcs) || ((e.length + lcs.length) === 0)
+    }
 
-    if(a[0] == b[0])
-      return [head(a)].concat(recurse(tail(a), tail(b)))
-    else { 
-      var _a = recurse(tail(a), b)
-      var _b = recurse(a, tail(b))
-      return cache[key(a,b)] = _a.length > _b.length ? _a : _b  
+    while(any(q, hasLength)) {
+      //if each element is at the lcs then this chunk is stable.
+      while(q.every(matchLcs) && q.every(hasLength)) 
+        all.forEach(retreat)
+
+      //collect the changes in each array upto the next match with the lcs
+      var c = false
+      var unstable = q.map(function (e) {
+        var change = []
+        while(!matchLcs(e)) {
+          change.unshift(retreat(e))
+          c = true
+        }
+        return change
+      })
+      if(c) build(q[0].length, unstable)
     }
   }
 
-  if(args.length > 2) {
-    //if called with multiple sequences
-    //recurse, since lcs(a, b, c, d) == lcs(lcs(a,b), lcs(c,d))
-    args.push(lcs(args.shift(), args.shift()))
-    return lcs(args)
-  }
-  return recurse(args[0], args[1])
-}
-
-// given n sequences, calc the lcs, and then chunk strings into stable and unstable sections.
-// unstable chunks are passed to build
-exports.chunk =
-function (q, build) {
-  var q = q.map(function (e) { return e.slice() })
-  var lcs = exports.lcs.apply(null, q)
-  var all = [lcs].concat(q)
-
-  function matchLcs (e) {
-    return last(e) == last(lcs) || ((e.length + lcs.length) === 0)
-  }
-
-  while(any(q, hasLength)) {
-    //if each element is at the lcs then this chunk is stable.
-    while(q.every(matchLcs) && q.every(hasLength)) 
-      all.forEach(retreat)
-
-    //collect the changes in each array upto the next match with the lcs
-    var c = false
-    var unstable = q.map(function (e) {
-      var change = []
-      while(!matchLcs(e)) {
-        change.unshift(retreat(e))
-        c = true
-      }
-      return change
+  exports.diff =
+  function (a, b) {
+    var changes = []
+    exports.chunk([a, b], function (index, unstable) {
+      var del = unstable.shift().length
+      var insert = unstable.shift()
+      changes.push([index, del].concat(insert))
     })
-    if(c) build(q[0].length, unstable)
+    return changes
+  }
+
+  exports.patch = function (a, changes, mutate) {
+    if(mutate !== true) a = a.slice(a)//copy a
+    changes.forEach(function (change) {
+      [].splice.apply(a, change)
+    })
+    return a
+  }
+
+  // http://en.wikipedia.org/wiki/Concestor
+  // me, concestor, you...
+  exports.merge = function () {
+    var args = getArgs(arguments)
+    var patch = exports.diff3(args)
+    return exports.patch(args[0], patch)
+  }
+
+  exports.diff3 = function () {
+    var args = getArgs(arguments)
+    var r = []
+    exports.chunk(args, function (index, unstable) {
+      var mine = unstable[0]
+      var insert = resolve(unstable)
+      if(equal(mine, insert)) return 
+      r.push([index, mine.length].concat(insert)) 
+    })
+    return r
+  }
+
+  var rules = exports.rules = [
+    function oddOneOut (changes) {
+      changes = changes.slice()
+      //put the concestor first
+      changes.unshift(changes.splice(1,1)[0])
+      var i = oddElement(changes, equal)
+      if(i == 0) // concestor was different, 'false conflict'
+        return changes[1]
+      if (~i)
+        return changes[i] 
+    },
+    //i've implemented this as a seperate rule,
+    //because I had second thoughts about this.
+    function insertMergeOverDelete (changes) {
+      changes = changes.slice()
+      changes.splice(1,1)// remove concestor
+      
+      //if there is only one non empty change thats okay.
+      //else full confilct
+      for (var i = 0, nonempty; i < changes.length; i++)
+        if(changes[i].length) 
+          if(!nonempty) nonempty = changes[i]
+          else return // full conflict
+      return nonempty
+    }
+  ]
+
+  function resolve (changes) {
+    var l = rules.length
+    for (var i in rules) { // first
+      var c = rules[i](changes)
+      if(c) return c
+    }
+    changes.splice(1,1) // remove concestor
+    return {'?': changes}
   }
 }
-
-exports.diff =
-function (a, b) {
-  var changes = []
-  exports.chunk([a, b], function (index, unstable) {
-    var del = unstable.shift().length
-    var insert = unstable.shift()
-    changes.push([index, del].concat(insert))
-  })
-  return changes
-}
-
-exports.patch = function (a, changes, mutate) {
-  if(mutate !== true) a = a.slice(a)//copy a
-  changes.forEach(function (change) {
-    [].splice.apply(a, change)
-  })
-  return a
-}
-
-// http://en.wikipedia.org/wiki/Concestor
-// me, concestor, you...
-exports.merge = function () {
-  var args = getArgs(arguments)
-  var patch = exports.diff3(args)
-  return exports.patch(args[0], patch)
-}
-
-exports.diff3 = function () {
-  var args = getArgs(arguments)
-  var r = []
-  exports.chunk(args, function (index, unstable) {
-    var mine = unstable[0]
-    var insert = resolve(unstable)
-    if(equal(mine, insert)) return 
-    r.push([index, mine.length].concat(insert)) 
-  })
-  return r
-}
-
-var rules = [
-  function oddOneOut (changes) {
-    changes = changes.slice()
-    //put the concestor first
-    changes.unshift(changes.splice(1,1)[0])
-    var i = oddElement(changes, equal)
-    if(i == 0) // concestor was different, 'false conflict'
-      return changes[1]
-    if (~i)
-      return changes[i] 
-  },
-  //i've implemented this as a seperate rule,
-  //because I had second thoughts about this.
-  function insertMergeOverDelete (changes) {
-    changes = changes.slice()
-    changes.splice(1,1)// remove concestor
-    
-    //if there is only one non empty change thats okay.
-    //else full confilct
-    for (var i = 0, nonempty; i < changes.length; i++)
-      if(changes[i].length) 
-        if(!nonempty) nonempty = changes[i]
-        else return // full conflict
-    return nonempty
-  }
-]
-
-function resolve (changes) {
-  var l = rules.length
-  for (var i in rules) { // first
-    var c = rules[i](changes)
-    if(c) return c
-  }
-  changes.splice(1,1) // remove concestor
-  return {'?': changes}
-}
-
+module.exports(module.exports, {equal: equal})
