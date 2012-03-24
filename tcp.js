c79592c6a6f34d86d1ac1511b8e6261c0746ebe5
Dominic Tarr <dominic.tarr@gmail.com>
2012-03-25 12:09:39 +1300

    attach names to the commits

--- tcp.js
+++ tcp.js
@@ -6,28 +6,27 @@ var s = new Repo()
 var c = new Repo()
 var _s
 
-function streamSync(update) {
+function streamSync(name, update) {
   var repo = new Repo()
 
   var synced = {
     commit: function (update) {
-      var commit = repo.commit(update, {parent: 'master'})
+      var commit = repo.commit(update, {parent: 'master', author: name})
       synced.send()
       synced.data = update
       if(synced.onupdate) synced.onupdate(update, 'this')
     },
     data: null,
+    repo: repo,
     handler: function (ins, out) {
       synced.ins = ins
       synced.out = out || ins 
       synced.out.pipe(es.split()).pipe(es.map(function (data) {
         data = JSON.parse(data)
-        console.log(data)
         try {
           var id = repo.recieve(data, 'master', true)
           repo.remote('other', 'master', id)
         } catch (err) {
-          console.log(data[0].changes.list)
           process.exit(1)
         }
         var data = synced.data = repo.checkout('master')
@@ -37,15 +36,12 @@ function streamSync(update) {
           //resolve conflicts
           if('object' == typeof data.list[l]) {
             var conflict = data.list[l]['?'][0]
-            console.log(data.list[l])
             ;[].splice.apply(data.list, [l, 1].concat(conflict)) 
             resolved = true
           }
         }
         if(resolved) {
           synced.commit(data)
-          console.log(data)
- //         process.exit(1)
         }
         if(synced.onupdate) synced.onupdate(synced.data, 'other')
         synced.send() // incase it was a merge
@@ -70,8 +66,8 @@ function streamSync(update) {
 var data = {
   list: []
 }
-var a = streamSync()
-var b = streamSync()
+var a = streamSync('A')
+var b = streamSync('B')
 var server = net.createServer(a.handler)
 server.listen(8282, function () {
   var client = net.createConnection(8282, function () {
@@ -80,8 +76,8 @@ server.listen(8282, function () {
 })
 
 setInterval(function () {
+  //return
   b.data.list.push(b.data.list.length)
-  console.log('B', JSON.stringify(b.data.list))
   b.commit(b.data)
 }, 1e3)
 
@@ -90,4 +86,5 @@ a.commit({list: ['crazy']})
 setInterval(function () {
   a.data.list.push('?')
   a.commit(a.data)
+  console.log(a.repo.getRevs('master'))
 }, 1e3)
