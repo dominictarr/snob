d883ce14cad0cae3cf6ec5634b4cdcede1bb3e50
Dominic Tarr <dominic.tarr@gmail.com>
2012-07-13 00:36:46 +1200

    use streams the right way

---
+++ stream.js
@@ -0,0 +1,109 @@
+/*
+  client sends greet.
+  server responds with updates since greet,
+  or sends thier own greet if they are the one behind.
+  
+  greet zero: clone
+  greet one: head
+  greet two: revlist.
+  
+  if you recieve clone. send all revs.
+  if you recieve a head, but don't have that. send revlist.
+  if you reiceve a revlist, send your updates since the concestor.
+
+  if there was an error, send the error, then end the stream.
+
+  idea: send sparse revlist, to maximise chance that you'll hit
+  bit minimise amount to send.
+
+  after the client has greeted, send any updates since the concestor
+*/
+var through = require('event-stream').through
+module.exports = function(repo, doGreet) {
+  var queue = []
+  var branch = 'master'
+  var stream
+  var greeted = false
+
+  function enqueue(revs) {
+    revs.forEach(function (e) {
+      queue.push(['update', e, branch])
+    })
+    stream.drain()
+  }
+  var remote = []
+  function ids (revs) {
+    return revs.map(function (e) {
+      return e.id
+    })
+  }
+  function remember (revs) {
+    revs.forEach(function (e) {
+      if(!~remote.indexOf(e.id))
+        remote.unshift(e.id)
+    })
+    console.log('REMOTE', remote)
+  }
+  function onUpdate () {
+    if(!greeted) return
+    if(stream.paused) return
+    var sendRevs = repo.getRevs(branch, remote)
+    if(!sendRevs.length) return
+    remember(sendRevs)
+    enqueue([sendRevs])
+  }
+  repo.on('update', onUpdate)
+  stream = through(function (data) {
+    var action  = data.shift()
+    var payload = data.shift()
+    var branch  = data.shift() || 'master'
+    if(action == 'greet') {
+      enqueue(repo.getRevs(branch, payload))
+      //now, can start sending updates
+      //repo.on('update',
+      greeted = true
+    } else if (action == 'update') {
+      remember(payload)
+      repo.recieve(payload, branch, true)
+    } 
+  }, function () {
+    if(!this.pause && !queue.length)
+      this.emit('end')
+  })
+  //called from the client.
+  stream.greet = function () {
+    if(greeted) return
+    greeted = true
+    queue.unshift(['greet', repo.revlist(branch), branch])
+    stream.drain()
+    return stream
+  }
+  var write = stream.write
+  stream.write = function (data) {
+    write.call(stream, data)
+    return true
+  }
+  stream.drain = function (resumed) {
+    while(!stream.paused && queue.length)
+      stream.emit('data', queue.shift())
+    if(!queue.length) {
+      if(resumed)    stream.emit('drain')
+      if(stream.ended) stream.emit('end')
+    }
+    return stream
+  }
+
+  stream.resume = function () {
+    stream.paused = false
+    onUpdate()
+    if(!queue.length) return
+    return stream.drain(true)
+  }
+  stream.queue = queue
+
+  if(doGreet !== false)
+    process.nextTick(stream.greet)
+
+  return stream
+}
+
---
+++ test/stream-test.js
@@ -0,0 +1,86 @@
+/*
+  connect two repos via streams,
+  and check that the repos update each other when not paused.
+*/
+
+
+var Repo = require('../')
+var createStream = require('../stream')
+var es = require('event-stream')
+var A = new Repo()
+var B = new Repo()
+
+var a = require('assertions')
+
+var as = A.createStream()
+var bs = B.createStream()
+
+console.log(as, bs, as == bs)
+as
+  .pipe(es.log('A-B->'))
+  .pipe(bs)
+  .pipe(es.log('B-A->'))
+  .pipe(as)
+
+as.greet().pause()
+
+var world
+
+A.commit({hello: ['hi']})
+A.commit(world = {hello: ['hi', 'HELLO']})
+
+console.log(A.checkout())
+console.log(B.checkout())
+
+a.notDeepEqual(A.checkout(), B.checkout())
+a.deepEqual(A.checkout(), world)
+
+as.resume()
+
+console.log(B.checkout())
+
+a.deepEqual(A.checkout(), B.checkout())
+B.commit(world = {hello: ['hi', 'hi, there', 'HELLO']})
+
+console.log(A.checkout())
+console.log(B.checkout())
+
+a.deepEqual(A.checkout(), B.checkout())
+a.deepEqual(A.checkout(), world)
+
+bs.pause()
+
+B.commit(world = {hello: ['hi', 'hi, there', 'Hello!']})
+
+a.notDeepEqual(A.checkout(), B.checkout())
+a.deepEqual(B.checkout(), world)
+
+bs.resume()
+
+console.log(A.checkout())
+console.log(B.checkout())
+
+a.deepEqual(A.checkout(), B.checkout())
+a.deepEqual(A.checkout(), world)
+
+bs.pause()
+as.pause()
+
+//merge conflict!
+
+B.commit({hello: ['hi', 'Hello!']})
+A.commit({hello: ['hi', 'Hello!', 'hullo']})
+
+console.log(A.checkout())
+console.log(B.checkout())
+
+console.log(A.checkout())
+console.log(B.checkout())
+
+bs.resume()
+as.resume()
+
+console.log(A.checkout())
+console.log(B.checkout())
+
+//wow, it worked.
