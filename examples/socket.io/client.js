83a304801426b4b2cab670dce448ffb10b62c6d7
Dominic Tarr <dominic.tarr@gmail.com>
2012-07-13 01:19:05 +1200

    the examples are all old and wrong, TODO: new examples

--- examples/socket.io/client.js
+++
@@ -1,9 +0,0 @@
-
-var bs = require('browser-stream')(io.connect('http://localhost:3000'))
-var Docuset = require('snob/docuset')
-var doc = new Docuset()
-
-SNOB = doc.createHandler()(bs.createStream({writable: true, readable: true, name: 'test'}))
-
-REPO = SNOB.sub('TEST')
-
